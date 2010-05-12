5d6c3742de4ff58568737b4f7f6d6e257274fb72
Daniel Parnell <me@danielparnell.com>
2010-05-13 09:10:45 +1000

    Initial commit

---
+++ init.el
@@ -0,0 +1,10 @@
+;; MuMaMo-Mode for rhtml files
+(add-to-list 'load-path "~/.emacs.d/nxhtml/util")
+(require 'mumamo-fun)
+(setq mumamo-chunk-coloring 'submode-colored)
+(add-to-list 'auto-mode-alist '("\\.rhtml\\'" . eruby-html-mumamo))
+(add-to-list 'auto-mode-alist '("\\.html\\.erb\\'" . eruby-html-mumamo))
+
+;; load rails-reloaded
+(setq load-path (cons (expand-file-name "~/.emacs.d/rails-reloaded") load-path))
+(require 'rails-autoload)
