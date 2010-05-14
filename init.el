ac66df0e7953d51fd94a25415ae1b2127aec34e1
Daniel Parnell <daniel@192-168-1-113.tpgi.com.au>
2010-05-14 22:05:22 +1000

    Fixed loading problems

--- init.el
+++ init.el
@@ -1,11 +1,11 @@
-;; MuMaMo-Mode for rhtml files
-
+;; emacs22 setup
 (if (< emacs-major-version 23)
       (setq load-path (append (append load-path '("~/.emacs.d/nxml/")) '("~/.emacs.d/ruby-mode"))))
 
+;; MuMaMo-Mode for rhtml files
 (load "~/.emacs.d/nxhtml/autostart.el")
-;; (add-to-list 'load-path "~/.emacs.d/nxhtml/util")
-(require 'mumamo-fun)
+;;(add-to-list 'load-path "~/.emacs.d/nxhtml/util")
+;;(require 'mumamo-fun)
 (setq mumamo-chunk-coloring 'submode-colored)
 (add-to-list 'auto-mode-alist '("\\.rhtml\\'" . eruby-html-mumamo))
 (add-to-list 'auto-mode-alist '("\\.html\\.erb\\'" . eruby-html-mumamo))
@@ -15,10 +15,10 @@
 (require 'rails-autoload)
 
 
-(rails/defbundle "Lib"
-  ()
-
-  (rails/defresource 'libs "Libs"
-                     :dir "lib"
-                     :file-ext  "*")
-)
+;;(rails/defbundle "Lib"
+;;  ()
+;;
+;;  (rails/defresource 'libs "Libs"
+;;                     :dir "lib"
+;;                     :file-ext  "*")
+;;)
