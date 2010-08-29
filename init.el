09eaaf40501f1b4783357aec56f461429a3ee323
Daniel Parnell <me@danielparnell.com>
2010-08-29 13:41:39 +1000

    Added scala mode and ensime

--- init.el
+++ init.el
@@ -14,10 +14,20 @@
 (setq load-path (cons (expand-file-name "~/.emacs.d/rails-reloaded") load-path))
 (require 'rails-autoload)
 
-;;(rails/defbundle "Lib"
-;;  ()
-;;
-;;  (rails/defresource 'libs "Libs"
-;;                     :dir "lib"
-;;                     :file-ext  "*")
-;;)
+;; scala-mode
+(let ((path "~/.emacs.d/scala-mode"))
+  (setq load-path (cons path load-path))
+  (load "scala-mode-auto.el"))
+
+(defun scala-turnoff-indent-tabs-mode ()
+  (setq indent-tabs-mode nil))
+
+;; scala mode hooks
+(add-hook 'scala-mode-hook 'scala-turnoff-indent-tabs-mode)
+
+;; ensime Scala stuff
+(require 'scala-mode)
+(add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
+(add-to-list 'load-path "~/.emacs.d/ensime_2.8.0-0.2.4/elisp/")
+(require 'ensime)
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
