b9d322a72aba199b464fa3fc0136ab411dd2c9c2
Daniel Parnell <me@danielparnell.com>
2011-10-25 17:04:42 +1100

    Lots of nice stuff

--- init.el
+++ init.el
@@ -17,8 +17,28 @@
 (add-to-list 'load-path "~/.emacs.d/nxhtml/util")
 (require 'mumamo-fun)
 (setq mumamo-chunk-coloring 'submode-colored)
-(add-to-list 'auto-mode-alist '("\\.rhtml\\'" . eruby-html-mumamo))
+; (add-to-list 'auto-mode-alist '("\\.rhtml\\'" . eruby-html-mumamo))
+; (add-to-list 'auto-mode-alist '("\\.html\\.erb\\'" . eruby-html-mumamo))
+
+(setq
+      nxhtml-global-minor-mode t
+      mumamo-chunk-coloring 'submode-colored
+      nxhtml-skip-welcome t
+      indent-region-mode t
+      rng-nxml-auto-validate-flag nil
+      nxml-degraded t)
 (add-to-list 'auto-mode-alist '("\\.html\\.erb\\'" . eruby-html-mumamo))
+(add-to-list 'auto-mode-alist '("\\.rhtml\\'" . eruby-html-mumamo))
+
+;; Special rails file
+(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
+
+;; RHTML mode
+;;; rhtml mode
+; (add-to-list 'load-path "~/.emacs.d/rhtml")
+; (require 'rhtml-mode)
 
 ;; load rails-reloaded
 (setq load-path (cons (expand-file-name "~/.emacs.d/rails-reloaded") load-path))
@@ -74,6 +94,22 @@
 
 (global-set-key "\C-cx" 'close-all-buffers)
 
+;; Cucumber mode
+(add-to-list 'load-path "~/.emacs.d/cucumber.el")
+(require 'feature-mode)
+(add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
+
+;; Coffee script mode
+(add-to-list 'load-path "~/.emacs.d/coffee-mode")
+(require 'coffee-mode)
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+
+;; SCSS mode
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/scss-mode"))
+(autoload 'scss-mode "scss-mode")
+(add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
+(setq scss-compile-at-save nil)
 
 ;; Set up mumao they way I want it
 (custom-set-variables
@@ -87,8 +123,23 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(mumamo-background-chunk-major ((((class color) (min-colors 88) (background light)) nil)))
+ '(mumamo-background-chunk-major ((((class color) (min-colors 87) (background light)) nil)))
  '(mumamo-background-chunk-submode1 ((((class color) (min-colors 88) (background light)) (:width normal))))
  '(mumamo-background-chunk-submode2 ((((class color) (min-colors 88) (background light)) (:weight normal))))
  '(mumamo-background-chunk-submode3 ((((class color) (min-colors 88) (background light)) nil)))
- '(mumamo-background-chunk-submode4 ((((class color) (min-colors 88) (background light)) nil))))
+ '(mumamo-background-chunk-submode4 ((((class color) (min-colors 88) (background light)) nil)))
+)
+
+;; Yaml mode
+(setq load-path (append load-path '("~/.emacs.d/yaml-mode")))
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-hook 'yaml-mode-hook
+	  '(lambda ()
+	     (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
+
+;; turn on mouse support in iTerm2
+(require 'mouse) 
+(xterm-mouse-mode t) 
+(defun track-mouse (e))
+
