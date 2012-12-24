53b47d271b68d588d7d7d78bcf915ed0be4aa6a7
Daniel Parnell <me@danielparnell.com>
2012-12-24 18:26:11 +1100

    Updated scala mode

--- init.el
+++ init.el
@@ -217,3 +217,7 @@
   "Connect to IRC."
   (interactive)
   (circe irc-host irc-port irc-net irc-password))
+
+(setq whitespace-action '(auto-cleanup)) ;; automatically clean up bad whitespace
+(setq whitespace-style '(trailing space-before-tab indentation empty space-after-tab)) ;; only show bad whitespace
+
