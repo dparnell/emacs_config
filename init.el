f05d30e132c10678a2c1a4475520acace3794e5d
Daniel Parnell <me@danielparnell.com>
2014-02-14 17:58:54 +1100

    Set indent width to 2 spaces for coffee-mode

--- init.el
+++ init.el
@@ -165,6 +165,7 @@
     (progn
         (add-to-list 'load-path "~/.emacs.d/coffee-mode")
         (require 'coffee-mode)
+        (custom-set-variables '(coffee-tab-width 2))
         (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
         (add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))))
 
