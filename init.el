3cc02f9fdc8df629e86328d20d1f8ab540f7d165
Daniel Parnell <me@danielparnell.com>
2014-08-01 07:17:12 +1000

    Added slime

--- init.el
+++ init.el
@@ -285,6 +285,15 @@
 (require 'auto-complete-config)
 (ac-config-default)
 
+;; SLIME
+;; setup load-path and autoloads
+(add-to-list 'load-path "~/.emacs.d/slime")
+(require 'slime-autoloads)
+ 
+;; Set your lisp system and, optionally, some contribs
+(setq inferior-lisp-program "/usr/local/bin/sbcl")
+(setq slime-contribs '(slime-fancy))
+
 ;; quicklisp support
 (when (file-exists-p "~/quicklisp/slime-helper.el")
   (progn
