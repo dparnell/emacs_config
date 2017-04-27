0b8448d427029733372ffe0b8aea6bf1b63dafd3
Daniel Parnell <me@danielparnell.com>
2017-04-27 17:28:06 +1000

    Fixed company-mode completion for CSS taking ages

--- init.el
+++ init.el
@@ -29,9 +29,9 @@
 (message "Loading required packages")
 (package-initialize)
 (let ((is-emacs-24-4-or-greater (or (> emacs-major-version 24) (and (= emacs-major-version 24) (> emacs-minor-version 3)))))
-  (let* ((packages-for-emacs-24-4-or-greater (if is-emacs-24-4-or-greater (list 'alchemist 'cider 'magit 'flycheck-elixir 'flycheck-clojure) (list)))
+  (let* ((packages-for-emacs-24-4-or-greater (if is-emacs-24-4-or-greater (list 'alchemist 'cider 'magit 'flycheck 'flycheck-elixir 'flycheck-clojure) (list)))
          (packages-for-emacs-24-or-greater (if (> emacs-major-version 23) (list 'coffee-mode 'company) (list)))
-         (common-packages (list 'clojure-mode 'iedit 'wgrep 'web-mode 'scss-mode 'yaml-mode 'json-mode))
+         (common-packages (list 'clojure-mode 'iedit 'wgrep 'web-mode 'scss-mode 'yaml-mode 'json-mode 'js2-mode))
          (to-install (delq nil (mapcar (lambda (x) (if (package-installed-p x) nil x)) (delq nil (append common-packages packages-for-emacs-24-or-greater packages-for-emacs-24-4-or-greater))))))
     (if to-install
         (progn
@@ -120,6 +120,9 @@
   ;; Use spaces instead of TABs
   (setq indent-tabs-mode nil)
   (setq c-indent-level 2)
+
+  ;; js2 mode
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
   ;; Special rails file
   (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
@@ -450,7 +453,16 @@
 
   ;; add company-mode
   (message "Adding company mode")
-  (add-hook 'after-init-hook 'global-company-mode)
+  ;; don't downcase company-mode completions
+  (setq company-dabbrev-downcase 0)
+  (defun setup-company-mode ()
+    (global-company-mode)
+    ;; fix company-mode css regex
+    (defconst company-css-property-value-regexp
+      "\\_<\\([[:alpha:]-]+\\):\\(?:[[:space:]]+\\)?\\([^{};]*\\_>\\|\\)\\="
+      "A regular expression matching CSS tags."))
+
+  (add-hook 'after-init-hook 'setup-company-mode)
 
   ;; add in Flycheck stuff
   (require 'flycheck-elixir)
