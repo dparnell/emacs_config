ba2e9d86a70ec9fd54b87b60d98a3cb647be4719
Daniel Parnell <me@danielparnell.com>
2017-07-22 13:35:59 +1000

    WIP: removing submodules

--- init.el
+++ init.el
@@ -29,7 +29,7 @@
 (let ((is-emacs-24-4-or-greater (or (> emacs-major-version 24) (and (= emacs-major-version 24) (> emacs-minor-version 3)))))
   (let* ((packages-for-emacs-24-4-or-greater (if is-emacs-24-4-or-greater (list 'alchemist 'cider 'magit 'flycheck 'flycheck-elixir 'flycheck-clojure) (list)))
          (packages-for-emacs-24-or-greater (if (> emacs-major-version 23) (list 'coffee-mode 'company 'yasnippet) (list)))
-         (common-packages (list 'clojure-mode 'iedit 'wgrep 'web-mode 'scss-mode 'yaml-mode 'json-mode 'js2-mode))
+         (common-packages (list 'clojure-mode 'iedit 'wgrep 'web-mode 'scss-mode 'yaml-mode 'json-mode 'js2-mode 'slime))
          (to-install (delq nil (mapcar (lambda (x) (if (package-installed-p x) nil x)) (delq nil (append common-packages packages-for-emacs-24-or-greater packages-for-emacs-24-4-or-greater))))))
     (if to-install
         (progn
@@ -342,8 +342,6 @@
   ;;(ac-config-default)
 
   ;; SLIME
-  ;; setup load-path and autoloads
-  (add-to-list 'load-path "~/.emacs.d/slime")
   (require 'slime-autoloads)
 
   ;; Set your lisp system and, optionally, some contribs
