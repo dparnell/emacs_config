45bf83671184ec262ca635f8011e085c52e97b5f
Daniel Parnell <me@danielparnell.com>
2017-07-22 13:58:25 +1000

    flymake-jslint

--- init.el
+++ init.el
@@ -29,7 +29,7 @@
 (let ((is-emacs-24-4-or-greater (or (> emacs-major-version 24) (and (= emacs-major-version 24) (> emacs-minor-version 3)))))
   (let* ((packages-for-emacs-24-4-or-greater (if is-emacs-24-4-or-greater (list 'alchemist 'cider 'magit 'flycheck 'flycheck-elixir 'flycheck-clojure) (list)))
          (packages-for-emacs-24-or-greater (if (> emacs-major-version 23) (list 'coffee-mode 'company 'yasnippet)
-                                                                          (list 'flymake-easy)))
+                                                                          (list 'flymake-easy 'flymake-jslint)))
          (common-packages (list 'clojure-mode 'iedit 'wgrep 'web-mode 'scss-mode 'yaml-mode 'json-mode 'js2-mode 'slime 'circe 'dockerfile-mode 'feature-mode 'ecb))
          (to-install (delq nil (mapcar (lambda (x) (if (package-installed-p x) nil x)) (delq nil (append common-packages packages-for-emacs-24-or-greater packages-for-emacs-24-4-or-greater))))))
     (if to-install
@@ -347,7 +347,6 @@
         ;; add flymake support for js
         (message "Loading flymake-easy")
         (require 'flymake-easy)
-        (add-to-list 'load-path "~/.emacs.d/flymake-jslint")
         (require 'flymake-jslint)
         (add-hook 'js-mode-hook 'flymake-jslint-load)
 
